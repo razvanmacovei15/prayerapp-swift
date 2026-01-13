822be42a02870131f523539b2d55a6de0c2bf811
razvanmc15 <r.macovei@aico.dev>
2026-01-13 09:27:56 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# prayerapp-swift
---
+++ prayerapp-swift/prayerapp_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  prayerapp_swiftApp.swift
+//  prayerapp-swift
+//
+//  Created by Razvan Macovei on 13.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct prayerapp_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
